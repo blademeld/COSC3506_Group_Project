0e18be47cfb724b019eeec912a34483c4ac91ef5
ryancad35 <122823664+ryancad35@users.noreply.github.com>
2026-03-16 19:16:20 -0400

    Add vertical layout to main window w/ subtitle

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,15 +1,21 @@
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
 	@Override
 	public void start(Stage stage) {
-		Label label = new Label("PeerLink Chat");
-		StackPane root = new StackPane(label);
+		Label titleLabel = new Label("PeerLink Chat");
+		Label subtitleLabel = new Label("Server-free peer-to-peer messaging application");
+
+		VBox root = new VBox(10);
+		root.setAlignment(Pos.CENTER);
+		root.getChildren().addAll(titleLabel, subtitleLabel);
+
 		Scene scene = new Scene(root, 700, 500);
 
 		stage.setTitle("PeerLink Chat");
