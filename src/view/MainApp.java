ee634b7404af9446710e07b8655c608bfe61fcd1
sangkeunkim <arbiternal@gmail.com>
2026-03-25 14:55:03 -0400

    Combined both views into one

--- src/view/MainApp.java
+++ src/view/MainApp.java
@@ -1,49 +1,23 @@
 package view;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.io.IOException;
 
 public class MainApp extends Application {
 	private static Stage primaryStage;
 
 	@Override
-	public void start(Stage stage) {
+	public void start(Stage stage) throws IOException {
 		primaryStage = stage;
-		try {
-			showConnectScene();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		primaryStage.show();
-	}
-
-	// Main connection scene where users can choose to host a session or connect to a peer
-	public static void showConnectScene() throws IOException {
-		FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("/view/MainApp.fxml"));
-		Scene scene = new Scene(loader.load(), 700, 500);
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/MainApp.fxml"));
+		Scene scene = new Scene(loader.load(), 900, 600);
 		primaryStage.setTitle("PeerLink Chat");
 		primaryStage.setScene(scene);
-	}
-
-	// Chat scene for hosting a session
-	public static void showHostChatScene(String portNumber) {
-		// Once networking is implemented , the host's IP address will be auto-grabbed and displayed here.
-		// For now, we will just display the port number.
-		ManagerView managerView = new ManagerView(portNumber);
-		Scene scene = managerView.getScene();
-		primaryStage.setTitle("PeerLink Chat - Manager Chat");
-		primaryStage.setScene(scene);
-	}
-
-	// Chat scene for connecting to a peer
-	public static void showConnectChatScene(String ipAddress, String portNumber) {
-		PeerView peerView = new PeerView(ipAddress, portNumber);
-		Scene scene = peerView.getScene();
-		primaryStage.setTitle("PeerLink Chat - Peer Chat");
-		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 	// Entry point of the application
